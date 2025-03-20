230eeabb89d0778a3d3dff5fbdd6025340ac5863
JM <JM@JOSEITSD>
2025-03-20 10:04:25 +0800

    Add project files.

---
+++ LibraryMS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LibraryMS.Startup))]
+namespace LibraryMS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
